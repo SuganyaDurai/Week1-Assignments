6b140c383b47e2c517b8151ef2e49cdc0d6c9125
SuganyaDurai <122260815+SuganyaDurai@users.noreply.github.com>
2023-01-13 19:31:44 +0530

    Add files via upload

--- MissingElementsinArray.java
+++ MissingElementsinArray.java
@@ -7,24 +7,33 @@ public class MissingElementsinArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Here is the input
-				int[] arr = {1,2,3,4,7,6,8};
+				int[] arr = {2,3,4,7,6,8};
 
 				// Sort the array
 				Arrays.sort(arr);
 				System.out.println("Sorted Array : ");					
 
 				// loop through the array (start i from arr[0] till the length of the array)
-				{
-					for (int i = 0; i < arr.length; i++) {
+				
+				for (int i = 0; i < arr.length; i++) {
 						System.out.print(arr[i]+ ", ");
 						
 					}
-					// check if the iterator variable is not equal to the array values respectively
-				}
+					// check if the iterator variable is not equal to the array values respectively	
+				int sum = 0;
 							
-				int sum = (arr.length+1)*(arr.length+2)/2;
+				
 				for (int i = 0; i < arr.length; i++) {
-					sum = sum - arr[i];
+					
+					  if(arr[i] + 1 == arr[i+1]) { 
+						  continue; 
+						  }else 
+						  {
+							  
+							 sum = arr[i]+1;
+							 break;
+							 }
+					 				
 					
 				}
 				System.out.println();
